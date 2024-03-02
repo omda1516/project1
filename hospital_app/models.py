fab8fd6e4df183a8992691bed4088e35cff0fb36
omda1516 <mohamed202119868@gmail.com>
2024-03-02 11:33:22 +0200

    test commit

--- hospital_app/models.py
+++ hospital_app/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+
+
 class Patient(models.Model):
     GENDER_TABLE =(
         ('M', 'Male'),
@@ -33,6 +35,11 @@ class Pharamcy(models.Model):
     def __str__(self):
         return f"{self.name} {self.location}"
 
+
+
+
+
+
 class Doctor(models.Model):  
     firstname = models.CharField(max_length=100)
     lastname = models.CharField(max_length=100)
@@ -43,15 +50,12 @@ class Doctor(models.Model):
     university= models.CharField(max_length=30)
     specialty= models.CharField(max_length=20)
     pharmacyID = models.ForeignKey(Pharamcy, on_delete=models.CASCADE,default=1)
-   
+    
     def __str__(self):
         return f"{self.firstname} {self.lastname}"
 
 
 
-
-
-    
 
 class Pharmacist(models.Model):  
     contact_number = models.CharField(max_length=15)
@@ -59,3 +63,53 @@ class Pharmacist(models.Model):
     pharmacyID = models.ForeignKey(Pharamcy, on_delete=models.CASCADE,default=1)
     def __str__(self):
         return f"{self.name} {self.location}"
+    
+
+
+
+class Refound(models.Model):
+    refound_amount=models.DecimalField(max_digits=10, decimal_places=10)
+
+
+
+
+
+class Reception(models.Model):  
+    name= models.CharField(max_length=20)
+    refound_id=models.ForeignKey(Refound,on_delete=models.CASCADE)
+
+
+
+class Reservation(models.Model):  
+    data = models.CharField(max_length=15)
+    payment_method= models.CharField(max_length=20)
+    payment_amount= models.CharField(max_length=20)
+    patientID = models.ForeignKey(Patient, on_delete=models.CASCADE,default=1)
+    doctorID = models.ForeignKey(Doctor, on_delete=models.CASCADE,default=1)
+    refound_id=models.ForeignKey(Refound,on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return f"{self.data} {self.payment_method}"
+    
+
+
+
+class management(models.Model):  
+    name = models.CharField(max_length=15)
+
+        
+    doctorID = models.ForeignKey(Doctor, on_delete=models.CASCADE,default=1)
+    refound_id=models.ForeignKey(Refound,on_delete=models.CASCADE)
+    Patient_id=models.ForeignKey(Patient,on_delete=models.CASCADE)
+    Reception_id=models.ForeignKey(Reception,on_delete=models.CASCADE)
+    Pharamcy_id=models.ForeignKey(Pharamcy,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.name}"
+    
+    # doctorID = models.ForeignKey(Doctor, on_delete=models.CASCADE,default=1)
+    # refound_id=models.ForeignKey(Refound,on_delete=models.CASCADE)
+    # Patient_id=models.ForeignKey(Patient,on_delete=models.CASCADE)
+    # Reception_id=models.ForeignKey(Reception,on_delete=models.CASCADE)
+    # Pharamcy_id=models.ForeignKey(Pharamcy,on_delete=models.CASCADE)
